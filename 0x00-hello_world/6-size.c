766f8738c05cc81012fc77f6d3938aef274fa015
rudye223 <rudyelisha7@gmail.com>
2023-02-16 12:24:22 -0800

    alx low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
@@ -12,4 +13,5 @@ printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
-}i
+}
+
